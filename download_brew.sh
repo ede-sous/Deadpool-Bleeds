5935dc6db6be501b2ff4f6c64c339f9fa9285069
Emanuel DE SOUSA ABRANTES <ede-sous@e1r6p21.42.fr>
2018-01-29 07:55:47 +0100

    fixing brew/sdl2 instalation

--- download_brew.sh
+++ download_brew.sh
@@ -1,1 +1,5 @@
+
+echo "source $HOME/.brewconfig.zsh" >> $HOME/.bashrc
+echo "source $HOME/.brewconfig.zsh" >> $HOME/.zshrc
+
 curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
--- download_sdl2.sh
+++ download_sdl2.sh
@@ -1,3 +1,4 @@
+
 brew install sdl2
 brew install sdl2_image
 brew install sdl2_ttf
